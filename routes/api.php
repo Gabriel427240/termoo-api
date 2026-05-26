1fe452bd07def39f6a0087e7e2bbd12a5f658418
Gabriel427240 <gabriel.costa6@alunos.unis.edu.br>
2026-05-26 19:21:35 -0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JogoController;
-
-Route::post('/jogos', [JogoController::class, 'iniciarJogo']);
-Route::post('/jogos/{idJogo}/tentativas', [JogoController::class, 'validarTentativa']);
